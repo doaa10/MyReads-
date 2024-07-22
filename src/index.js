72947854520a2b446e864dbed5c76b9a34dd3a67
doaa <73131139+doaa10@users.noreply.github.com>
2024-07-22 04:24:44 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-=======
 import {BrowserRouter} from 'react-router-dom';
->>>>>>> master
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
